03931f4c3dd6a438e65f3218abea6eb17fec397d
vasanthareddy21 <vasanthareddya9@gamil.com>
2023-09-28 09:01:28 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from app.forms import *
 from django.core.mail import send_mail
 
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 def registration(request):
@@ -29,4 +32,29 @@ def registration(request):
     return render(request,'registration.html',d)
 
 def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
     return render(request,'home.html')
+
+def user_login(request):
+    if(request.method=='POST'):
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active :
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('login credentials are not correct')
+    return render(request,'user_login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+    
+
